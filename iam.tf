abc89f452340e9277f365e0fe19c8376161aa303
Ra'Jiska <dodo.lasticot@gmail.com>
2023-11-11 20:27:21 +0800

    Config Typo + Metric IAM Perms

--- iam.tf
+++ iam.tf
@@ -74,6 +74,26 @@ data "aws_iam_policy_document" "main" {
       ]
     }
   }
+
+  dynamic "statement" {
+    for_each = var.use_cloudwatch_agent ? ["x"] : []
+
+    content {
+      sid    = "CWAgentMetrics"
+      effect = "Allow"
+      actions = [
+        "cloudwatch:PutMetricData"
+      ]
+      resources = [
+        "*"
+      ]
+      condition {
+        test     = "StringEquals"
+        variable = "cloudwatch:namespace"
+        values   = [var.cloudwatch_agent_configuration.namespace]
+      }
+    }
+  }
 }
 
 resource "aws_iam_role" "main" {

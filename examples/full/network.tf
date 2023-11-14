d0131027c6f9a27f0dde5a1c09a33f7716a143f0
Ra'Jiska <dodo.lasticot@gmail.com>
2023-11-14 22:13:36 +0800

    Pre-commit hook for fmt

--- examples/full/network.tf
+++ examples/full/network.tf
@@ -35,7 +35,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route" "public_igw" {
-	route_table_id         = aws_route_table.public.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 

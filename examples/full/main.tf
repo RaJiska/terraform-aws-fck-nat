e66578bec25da94d62f665ae063570ba0765a859
Renato Golia <renato.golia@insightarchitectures.com>
2023-12-03 16:15:18 +0100

    Restored sample

--- examples/full/main.tf
+++ examples/full/main.tf
@@ -1,10 +1,6 @@
 locals {
   name     = "fck-nat-example"
   vpc_cidr = "10.255.255.0/24"
-
-  trusted_networks = [
-    "123.123.123.123/32"
-  ]
 }
 
 data "aws_region" "current" {}
@@ -20,18 +16,5 @@ module "fck-nat" {
   update_route_tables = true
   route_tables_ids = {
     "private" = aws_route_table.private.id
-  }
-
-  additional_security_group_ids = [aws_security_group.additional_security_group.id]
-}
-
-resource "aws_security_group" "additional_security_group" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = local.trusted_networks
   }
 }

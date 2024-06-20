9377bf9247c96318b99273eb2978d1afce8cf0eb
Doriann Corlouër <RaJiska@users.noreply.github.com>
2024-06-20 19:50:49 +0900

    Name tag takes precedence over Name variable (#26)

--- asg.tf
+++ asg.tf
@@ -13,10 +13,14 @@ resource "aws_autoscaling_group" "main" {
     version = "$Latest"
   }
 
-  tag {
-    key                 = "Name"
-    value               = var.name
-    propagate_at_launch = true
+  dynamic "tag" {
+    for_each = lookup(var.tags, "Name", null) == null ? ["Name"] : []
+
+    content {
+      key                 = "Name"
+      value               = var.name
+      propagate_at_launch = true
+    }
   }
 
   dynamic "tag" {

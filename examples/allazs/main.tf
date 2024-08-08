ff2e2da489f911bd415bff82f05af79aab8d3fe2
Bob Sutterfield <bobsut@users.noreply.github.com>
2024-08-08 16:46:49 -0700

    clean up names, add deployment examples

--- examples/allazs/main.tf
+++ examples/allazs/main.tf
@@ -17,9 +17,13 @@ module "fck-nat" {
   source   = "../../"
   for_each = toset(data.aws_availability_zones.azs.zone_ids)
 
-  name      = "${local.name}-nat-${each.key}"
+  name      = "${local.name}-${each.key}"
   vpc_id    = aws_vpc.main.id
   subnet_id = aws_subnet.public[each.key].id
+  # ha_mode              = true
+  # use_cloudwatch_agent = true
+  # use_spot_instances   = true
+  # instance_type        = "t4g.nano"
 
   update_route_tables = true
   route_tables_ids = {
@@ -27,6 +31,6 @@ module "fck-nat" {
   }
 
   tags = {
-    Name = "${local.name}-fck-nat-${each.key}"
+    Name = "${local.name}-${each.key}"
   }
 }

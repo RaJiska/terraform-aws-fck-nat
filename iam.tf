fe33cea11ad4120cb7162e51cdca361031eb07a7
Bob Sutterfield <bobsut@users.noreply.github.com>
2024-10-03 18:12:05 -0700

    remove inline_policy argument

--- iam.tf
+++ iam.tf
@@ -118,24 +118,28 @@ data "aws_iam_policy_document" "main" {
 resource "aws_iam_role" "main" {
   name = var.name
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  inline_policy {
-    name   = "Main"
-    policy = data.aws_iam_policy_document.main.json
-  }
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
 
   tags = var.tags
+}
+
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "main" {
+  name   = var.name
+  role   = aws_iam_role.main.id
+  policy = data.aws_iam_policy_document.main.json
+}
+
+resource "aws_iam_role_policies_exclusive" "main" {
+  role_name    = aws_iam_role.main.name
+  policy_names = [aws_iam_role_policy.main.name]
 }
